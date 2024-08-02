688c01fdef827748a8cce2eb8f45de0e9a293b4d
Angel Vazquez <angelvazquez470@gmail.com>
2024-08-01 19:21:48 -0600

    create de productos terminado. trabajando en index de productos

---
+++ app/Http/Controllers/MeasureUnitController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MeasureUnit;
+use Illuminate\Http\Request;
+
+class MeasureUnitController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:100',
+        ]);
+
+        MeasureUnit::create($request->all());
+    }
+
+    public function show(MeasureUnit $measureUnit)
+    {
+        //
+    }
+
+    public function edit(MeasureUnit $measureUnit)
+    {
+        //
+    }
+
+    public function update(Request $request, MeasureUnit $measureUnit)
+    {
+        //
+    }
+
+    public function destroy(MeasureUnit $measureUnit)
+    {
+        //
+    }
+}
