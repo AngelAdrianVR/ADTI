314320a2f1943685eb2bf0d086e8d5f291ff5480
Miguel Vázquez <miguelvz26.mv@gmail.com>
2024-12-13 10:31:43 -0600

    Incidencias
    • Mejora en comando para sincronizar  incidencias desde bio time

---
+++ app/Http/Controllers/BioTimeTransactionsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BioTimeTransactions;
+use Illuminate\Http\Request;
+
+class BioTimeTransactionsController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(BioTimeTransactions $bioTimeTransactions)
+    {
+        //
+    }
+
+    public function edit(BioTimeTransactions $bioTimeTransactions)
+    {
+        //
+    }
+
+    public function update(Request $request, BioTimeTransactions $bioTimeTransactions)
+    {
+        //
+    }
+
+    public function destroy(BioTimeTransactions $bioTimeTransactions)
+    {
+        //
+    }
+}
