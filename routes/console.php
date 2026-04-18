5b2128099cff18f510800be13d5d821a8656dcca
Miguel Vázquez <miguelvz26.mv@gmail.com>
2026-04-17 19:26:09 -0600

    Detalles de sincronización
    • Se crea nueva version de sincronizador de python con el método de 'Aguja de tiempo'
    • Se corrige anti rafaga al registrar salidas

--- routes/console.php
+++ routes/console.php
@@ -2,19 +2,22 @@
 
 use App\Models\Payroll;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('payrolls:close')
     ->tuesdays() 
-    ->at('00:00') // Ejecutar exactamente a las 12:00 AM
+    ->at('01:00') // Ejecutar exactamente a las 1:00 AM
     ->timezone('America/Mexico_City') // Forzar tu zona horaria local
     ->when(function () {
         $activePayroll = Payroll::firstWhere('is_active', true);
 
         if (!$activePayroll || !$activePayroll->start_date) {
+            Log::warning('No hay nómina activa o no tiene start_date, no se puede cerrar la nómina.');
             return false; // No hay nómina activa o no tiene start_date
         }
 
+        Log::info(Carbon::parse($activePayroll->start_date)->diffInDays(now()) . ' días han pasado desde el inicio de la nómina activa.');
         // Calcula si han pasado al menos 8 días desde start_date
         return Carbon::parse($activePayroll->start_date)->diffInDays(now()) > 8;
     });
