97ea58c94d1525ff672d2323bc0bde3f9ef82447
Miguel Vázquez <miguelvz26.mv@gmail.com>
2026-04-29 13:24:54 -0600

    Detalles de cronjob

--- routes/console.php
+++ routes/console.php
@@ -5,10 +5,13 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schedule;
 
+Schedule::command('users:update-vacations')
+    ->dailyAt('01:00');
+
 Schedule::command('payrolls:close')
-    ->tuesdays() 
+    ->tuesdays()
     ->at('01:00') // Ejecutar exactamente a las 1:00 AM
-    ->timezone('America/Mexico_City') // Forzar tu zona horaria local
+    // ->timezone('America/Mexico_City') // Forzar tu zona horaria local
     ->when(function () {
         $activePayroll = Payroll::firstWhere('is_active', true);
 
@@ -21,9 +24,3 @@ Schedule::command('payrolls:close')
         // Calcula si han pasado al menos 8 días desde start_date
         return Carbon::parse($activePayroll->start_date)->diffInDays(now()) > 8;
     });
-
-// De paso, le aplicamos la misma regla a las vacaciones para que se calculen en la madrugada local
-Schedule::command('users:update-vacations')
-    ->dailyAt('01:00')
-    ->timezone('America/Mexico_City');
-// Schedule::command('payrolls:sync-incidents')->everyMinute();
